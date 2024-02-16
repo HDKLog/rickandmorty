a1f0734651c9793876afb2aed9ff07df39572849
Harry S <gari_sarkisyan@epam.com>
2024-02-16 19:30:52 +0400

    initial

---
+++ rickandmorty/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  rickandmorty
+//
+//  Created by Gari Sarkisyan on 16.02.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ rickandmorty/rickandmortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  rickandmortyApp.swift
+//  rickandmorty
+//
+//  Created by Gari Sarkisyan on 16.02.24.
+//
+
+import SwiftUI
+
+@main
+struct rickandmortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
